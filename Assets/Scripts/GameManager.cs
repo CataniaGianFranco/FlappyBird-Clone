ddbbcffebe5efe13024e4bc10fecd46a38e8f863
Gian <catania.gian@gmail.com>
2024-04-24 15:41:41 -0300

    Updated code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text _scoreText = null;
+       
     private static GameManager _instance;
-    public static GameManager Instance { get { return _instance; } }
-
     private bool _isGameOver = false;
+    private int _score = 0;
+
+    
+    public static GameManager Instance { get { return _instance; } }
     public bool IsGameOver { get { return _isGameOver; } }
 
     private void Awake()
     {
         Singleton();
-    }
-
-    private void Update()
-    {
-        
     }
 
     private void Singleton()
@@ -26,7 +26,13 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
     }
 
-    public void Gameover()
+    public void IncreaseScore()
+    {
+        _score++;
+        _scoreText.text = _score.ToString();
+    }
+
+    public void GameOver()
     {
         _isGameOver = true;
     }
