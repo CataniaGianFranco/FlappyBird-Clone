98fcedd26e69775a3d2cf45090741dae329f2d42
Gian <catania.gian@gmail.com>
2024-04-24 11:42:29 -0300

    Generate obstacles

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class Generator : MonoBehaviour
 {
     [SerializeField] private GameObject _pipePrefab = null;
+    private GameObject[] _obstacles = null;
+    private float xSpawnPosition = 12.0f;
+    private float ySpawnTopPosition = 3.25f;
+    private float ySpawnBottomPosition = -1.25f;
     private int _poolSize = 5;
-    private float _spawnTimer = 2.5f;
+    private int _obstacleCount = 0;
+    private float _timeElapsed = 0.0f;
+    private float _spawnTime = 2.5f;
 
     private void Start()
     {
@@ -13,17 +19,43 @@ public class Generator : MonoBehaviour
 
     private void Update()
     {
-        
+        SpawnWithTimer();
     }
 
     private void GenerateObstacles()
     {
+        _obstacles = new GameObject[_poolSize];
+
         for (int i = 0; i < _poolSize; i++)
         {
-            GameObject _pipe = Instantiate<GameObject>(_pipePrefab);
-            _pipe.SetActive(false);
-            _pipe.gameObject.transform.SetParent(this.transform);
-            _pipe.transform.position = new Vector3(12.0f, 0.0f, 0.0f);
+            _obstacles[i] = Instantiate<GameObject>(_pipePrefab);
+            _obstacles[i].gameObject.transform.SetParent(this.transform);
+            _obstacles[i].SetActive(false);
         }
+    }
+
+    private void SpawnWithTimer()
+    {
+        _timeElapsed += Time.deltaTime;
+
+        if (_timeElapsed > _spawnTime && !GameManager.Instance.IsGameOver)
+            SpawnObstacle();
+    }
+
+    private void SpawnObstacle()
+    {
+        _timeElapsed = 0.0f;
+
+        float ySpawnPosition = Random.Range(ySpawnBottomPosition, ySpawnTopPosition);
+        Vector3 spawnPosition = new Vector3(xSpawnPosition, ySpawnPosition, 0.0f);
+        _obstacles[_obstacleCount].transform.position = spawnPosition;
+        
+        if (!_obstacles[_obstacleCount].activeSelf)
+            _obstacles[_obstacleCount].SetActive(true);
+
+        _obstacleCount++;
+
+        if (_obstacleCount == _poolSize)
+            _obstacleCount = 0;
     }
 }
